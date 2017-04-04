3250e850c76d9ccbb865ffb7c2c43b9cda918bd5
Cooper <alison@betterment.com>
2017-04-04 19:55:22 -0400

    basic spring boot setup, readme, config

---
+++ README.md
@@ -0,0 +1,15 @@
+# Quartet 
+
+## Running the App
+
+Execute ```./gradlew build && java -jar build/libs/gs-spring-boot-0.1.0.jar``` from within the 
+app directory.
+
+## Overview
+
+TODO
+
+
+## Tests
+
+TODO
---
+++ src/main/java/com/quartet/QuartetApplication.java
@@ -0,0 +1,13 @@
+package com.quartet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartetApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(QuartetApplication.class, args);
+    }
+}
